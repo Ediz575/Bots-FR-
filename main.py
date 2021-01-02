ee26cada985aba17d9a0654e6e2bf343accd38de
Ediz575 <76855027+Ediz575@users.noreply.github.com>
2021-01-02 15:55:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import discord
+from discord.ext import commands
+bot = commands.Bot(command_prefix = "+")
+
+@bot.event
+async def on_ready():
+	  print("Ready +")
+
+	  client.run("Nzg0NTE1MzcwNTY3OTkxMzQ3.X8qa3w.X1CMuvCOG7oRG5Io-tfVucj2uws")
